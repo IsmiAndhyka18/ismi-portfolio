adc001547a7d9c3456187c33d4363d448eca08ba
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-04-23 18:41:54 +0200

    fixing the responsive

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -9,10 +10,7 @@ hamburger.addEventListener("click", () => {
   });
 });
 
-AOS.init({
-  offset : 370,
-  duration: 1000
-});
+
 
 
 

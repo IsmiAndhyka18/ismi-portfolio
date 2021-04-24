c4e43a7c7a102bbac8b725ac99eb2ba967ff4cd2
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-04-24 21:23:10 +0200

    Adding fadein animation

--- script.js
+++ script.js
@@ -2,17 +2,33 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-// AOS.init({
-//   offset: 370,
-//   duration: 1000,
-// });
-
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
-
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-      navLinks.classList.remove("open");
-    });
+  links.forEach(link => {
+    link.classList.toggle("fade");
   });
 });
+
+function fadeOut(el) {
+  el.style.opacity = 1;
+  (function fade() {
+      if ((el.style.opacity -= .1) < 0) {
+          el.style.display = "none";
+      } else {
+          requestAnimationFrame(fade);
+      }
+  })();
+};
+
+// ** FADE IN FUNCTION **
+function fadeIn(el, display) {
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+  (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += .1) > 1)) {
+          el.style.opacity = val;
+          requestAnimationFrame(fade);
+      }
+  })();
+};

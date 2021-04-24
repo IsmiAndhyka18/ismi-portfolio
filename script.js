523ec5e224237856568cc67d1c9a5ba161746940
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-04-24 21:30:58 +0200

    fixing the animation

--- script.js
+++ script.js
@@ -9,26 +9,22 @@ hamburger.addEventListener("click", () => {
   });
 });
 
-function fadeOut(el) {
-  el.style.opacity = 1;
-  (function fade() {
-      if ((el.style.opacity -= .1) < 0) {
-          el.style.display = "none";
-      } else {
-          requestAnimationFrame(fade);
-      }
-  })();
-};
+var opacity = 0;
+var intervalID = 0;
+window.onload = fadeIn;
 
-// ** FADE IN FUNCTION **
-function fadeIn(el, display) {
-  el.style.opacity = 0;
-  el.style.display = display || "block";
-  (function fade() {
-      var val = parseFloat(el.style.opacity);
-      if (!((val += .1) > 1)) {
-          el.style.opacity = val;
-          requestAnimationFrame(fade);
-      }
-  })();
-};
+function fadeIn() {
+    setInterval(show, 200);
+}
+
+function show() {
+    var body = document.getElementById("body");
+    opacity = Number(window.getComputedStyle(body)
+                     .getPropertyValue("opacity"));
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        body.style.opacity = opacity
+    } else {
+        clearInterval(intervalID);
+    }
+}
